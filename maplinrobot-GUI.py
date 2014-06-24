3383104b0384a7c2e1e83206e89b3e37fef00ed3
root <root@raspberrypi.(none)>
2014-06-24 09:36:40 +0000

    Introducida la lista y los menus  de ordenes

--- maplinrobot-GUI.py
+++ maplinrobot-GUI.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
 from Tkinter import *
+from tkFileDialog import askopenfilename, asksaveasfilename
 from ttk import *
 import random
+import pickle 
 
 from comunicacion_con_brazo import comunicacion
 
 class App:
+	
 	def __init__(self, master):
 		"""iniciando la comunicacion con el brazo"""
 		self.com=comunicacion()
@@ -15,80 +18,90 @@ class App:
 		self.master = master
 		self.tiempo = DoubleVar()
 		self.ordenesGrabadas=[]		#Ejemplo: ordenesGrabadas=[("base-izquierda", 1), ("base-derecha",1)]
-		self.cadenaOrdenes=""
-		self.ordenesGrabadasString=StringVar()
 		self.enGrabacion=BooleanVar()
 		self.duration=1.0 # Duration (In seconds) for each action. Defaults to 1 second
-		
 		#---------------Ambiente Grafico- -----------#
-		mainframe = Frame(master, padding = '3 3 12 12')
+		self.mainframe = Frame(self.master, padding = '3 3 12 12')
+		self.makeWidgets()
+		
+	
+	def makeWidgets(self):
+		
 		#hace que el marco se empaquete en el espacio disponible
 	
-		mainframe.grid(column=0, row= 0, sticky=(N,W,E,S))
-		mainframe.columnconfigure(0, weight=1)
-		mainframe.rowconfigure(0, weight=1)
+		self.mainframe.grid(column=0, row= 0, sticky=(N,W,E,S))
+		self.mainframe.columnconfigure(0, weight=1)
+		self.mainframe.rowconfigure(0, weight=1)
 		
 		#pinza
-		Label(mainframe, text='Pinza').grid(column=0, row=0, sticky=W)
-		Button(mainframe, text="Abrir", command = lambda : self.actuar('abrir-pinza') ).grid(column=1, row=0, sticky=W)
-		Button(mainframe, text="Cerrar", command = lambda : self.actuar('cerrar-pinza')).grid(column=1, row=1, sticky=W)
+		Label(self.mainframe, text='Pinza').grid(column=0, row=0, sticky=W)
+		Button(self.mainframe, text="Abrir", command = lambda : self.actuar('abrir-pinza')).grid(column=1, row=0, sticky=W)
+		Button(self.mainframe, text="Cerrar", command = lambda : self.actuar('cerrar-pinza')).grid(column=1, row=1, sticky=W)
 		
 		#separador verticales
-		Label(mainframe, text='     ').grid(column=2, row=1, sticky=W)
+		Label(self.mainframe, text='     ').grid(column=2, row=1, sticky=W)
 		
 		#base
-		Label(mainframe, text='Base').grid(column=3, row=0, sticky=E)
-		Button(mainframe, text="derecha", command = lambda : self.actuar('base-derecha') ).grid(column=4, row=0, sticky=W)
-		Button(mainframe, text="izquierda", command = lambda : self.actuar('base-izquierda')).grid(column=4, row=1, sticky=W)
+		Label(self.mainframe, text='Base').grid(column=3, row=0, sticky=E)
+		Button(self.mainframe, text="derecha", command = lambda : self.actuar('base-derecha') ).grid(column=4, row=0, sticky=W)
+		Button(self.mainframe, text="izquierda", command = lambda : self.actuar('base-izquierda')).grid(column=4, row=1, sticky=W)
 		
 		#separador vertical
-		Label(mainframe, text='     ').grid(column=5, row=1, sticky=W)
+		Label(self.mainframe, text='     ').grid(column=5, row=1, sticky=W)
 	
 		#hombro
-		Label(mainframe, text='Hombro').grid(column=6, row=0, sticky=E)
-		Button(mainframe, text="subir", command = lambda : self.actuar('subir-hombro')).grid(column=7, row=0, sticky=W)
-		Button(mainframe, text="bajar", command = lambda : self.actuar('bajar-hombro')).grid(column=7, row=1, sticky=W)
+		Label(self.mainframe, text='Hombro').grid(column=6, row=0, sticky=E)
+		Button(self.mainframe, text="subir", command = lambda : self.actuar('subir-hombro')).grid(column=7, row=0, sticky=W)
+		Button(self.mainframe, text="bajar", command = lambda : self.actuar('bajar-hombro')).grid(column=7, row=1, sticky=W)
 		
 		#codo
-		Label(mainframe, text='Codo').grid(column=0, row=4, sticky=W)
-		Button(mainframe, text="subir", command = lambda : self.actuar('subir-codo')).grid(column=1, row=4, sticky=W)
-		Button(mainframe, text="bajar", command = lambda : self.actuar('bajar-codo')).grid(column=1, row=5, sticky=W)
+		Label(self.mainframe, text='Codo').grid(column=0, row=4, sticky=W)
+		Button(self.mainframe, text="subir", command = lambda : self.actuar('subir-codo')).grid(column=1, row=4, sticky=W)
+		Button(self.mainframe, text="bajar", command = lambda : self.actuar('bajar-codo')).grid(column=1, row=5, sticky=W)
 		
 		#munneca
-		Label(mainframe, text='Munneca').grid(column=3, row=4, sticky=W)
-		Button(mainframe, text="subir", command = lambda : self.actuar('subir-munneca')).grid(column=4, row=4, sticky=W)
-		Button(mainframe, text="bajar", command = lambda : self.actuar('bajar-munneca')).grid(column=4, row=5, sticky=W)
+		Label(self.mainframe, text='Munneca').grid(column=3, row=4, sticky=W)
+		Button(self.mainframe, text="subir", command = lambda : self.actuar('subir-munneca')).grid(column=4, row=4, sticky=W)
+		Button(self.mainframe, text="bajar", command = lambda : self.actuar('bajar-munneca')).grid(column=4, row=5, sticky=W)
 		
 		#separador horizontal
-		Label(mainframe, text=' ').grid(column=3, row=3, sticky=W)
+		Label(self.mainframe, text=' ').grid(column=3, row=3, sticky=W)
 		
 		#luz
-		Label(mainframe, text='Luz').grid(column=6, row=4, sticky=E)
-		Button(mainframe, text="encender", command = lambda : self.actuar('encender-luz')).grid(column=7, row=4, sticky=W)
-		Button(mainframe, text="apagar", command = lambda : self.actuar('apagar-luz')).grid(column=7, row=5, sticky=W)
+		Label(self.mainframe, text='Luz').grid(column=6, row=4, sticky=E)
+		Button(self.mainframe, text="encender", command = lambda : self.actuar('encender-luz')).grid(column=7, row=4, sticky=W)
+		Button(self.mainframe, text="apagar", command = lambda : self.actuar('apagar-luz')).grid(column=7, row=5, sticky=W)
 		
 		#grabacion
-		Label(mainframe, text='Grabacion').grid(column=0, row=8, sticky=W)
-		Button(mainframe, text="Play", command = self.ejecutarOrdenesGrabadas).grid(column=0, row=9, sticky=W)
-		Button(mainframe, text="Grabar", command = self.iniciarGrabacion).grid(column=1, row=9, sticky=W)
+		Label(self.mainframe, text='Grabacion').grid(column=0, row=8, sticky=W)
+		Button(self.mainframe, text="Ejecutar Grabacion", command = self.ejecutarOrdenesGrabadas).grid(column=0, row=9, sticky=W)
+		Button(self.mainframe, text="Parar de Grabar", command = self.pararGrabacion).grid(column=0, row=10, sticky=W)
+		Button(self.mainframe, text="Grabar", command = self.iniciarGrabacion).grid(column=1, row=9, sticky=W)
+		Button(self.mainframe, text="Guardar", command = self.guardarGrabacion).grid(column=2, row=9, sticky=W)
+		Button(self.mainframe, text="Cargar", command = self.cargarGrabacion).grid(column=3, row=9, sticky=W)
+		Button(self.mainframe, text="Borrar", command = self.borrarOrdenesGrabadas).grid(column=4, row=9, sticky=W)
 		
-		Label(mainframe, text='Estado Grabacion ').grid(column=0, row=10, sticky=W)
-		Label(mainframe, textvariable=self.enGrabacion).grid(column=0, row=11, sticky=W)
+		Label(self.mainframe, text='Estado Grabacion ').grid(column=0, row=11, sticky=W)
+		Label(self.mainframe, textvariable=self.enGrabacion).grid(column=0, row=12, sticky=W)
 	
 		
 		#ordenes
-		Label(mainframe, text='ordenes').grid(column=7, row=7, sticky=W)
-		message = Message(mainframe, textvariable=self.ordenesGrabadasString)
-		message.grid(row=8,column=7, sticky=E)
-	
+		Label(self.mainframe, text='ordenes grabadas').grid(column=7, row=7, sticky=W)
+		list = Listbox(self.mainframe, relief=SUNKEN)
+		list.grid(row=8,column=7, sticky=E)	
+		#list.config(selectmode=SINGLE, setgrid=1)
+		list.bind('<Double-1>', self.handleList)
+		self.listaOrdenes = list
+		
+		
 		#separador horizontal
-		Label(mainframe, text='    ').grid(column=0, row=5, sticky=W)
+		Label(self.mainframe, text='    ').grid(column=0, row=5, sticky=W)
 		
 		#tiempo deslizable
-		Label(mainframe, text='Tiempo').grid(column=0, row=6, sticky=W)
-		scale=Scale(mainframe, from_=1, to=4, orient=HORIZONTAL,
+		Label(self.mainframe, text='Tiempo').grid(column=0, row=6, sticky=W)
+		scale=Scale(self.mainframe, from_=1, to=4, orient=HORIZONTAL,
 			command=self.onScale).grid(column=0, row=7)
-		Label(mainframe, textvariable=self.tiempo,
+		Label(self.mainframe, textvariable=self.tiempo,
 			font=("Helvetica", 15)).grid(row=7, column=1,sticky=E)
 					
 	# ---------------------- Acciones -------------------------	
@@ -102,7 +115,7 @@ class App:
 		self.enGrabacion es True """
 		self.com.MoveArm(t=self.tiempo.get(), cmd=comando)
 		if self.enGrabacion.get():
-			self.actualizarOrdenesGrabadas(comando)
+			self.actualizarOrdenesGrabadas(comando,self.tiempo.get())
 			
 	
 	# -----------------------------------------------------------	
@@ -115,11 +128,10 @@ class App:
 	def pararGrabacion(self):
 		self.enGrabacion.set(False)
 	
-	def actualizarOrdenesGrabadas(self,comando):
-		self.ordenesGrabadas.append((comando, self.tiempo.get()))
-		self.cadenaOrdenes += comando + "\n"
-		self.ordenesGrabadasString.set(self.cadenaOrdenes)
-		print self.cadenaOrdenes
+	def actualizarOrdenesGrabadas(self,comando, tiempo):
+		self.ordenesGrabadas.append((comando, tiempo))
+		self.listaOrdenes.insert(END, comando + " " + str(int(tiempo)) + " seg")
+		#print self.ordenesGrabadas
 	
 	def ejecutarOrdenesGrabadas(self):
 		self.pararGrabacion()
@@ -128,8 +140,40 @@ class App:
 		for orden in self.ordenesGrabadas:
 			self.tiempo.set(orden[1])
 			self.actuar(orden[0])
-	
-	
+			
+	def guardarGrabacion(self):
+		archivo = asksaveasfilename()		
+		try:
+			with open(archivo, 'wb') as mysavedata:
+				pickle.dump(self.ordenesGrabadas, mysavedata)
+		except IOError as err:
+			print('File error: ' + str(err))
+		except pickle.PickleError as perr:
+			print('Pickling error: ' + str(perr))
+		
+	def cargarGrabacion(self):
+		archivo = askopenfilename()
+		try:
+			with open(archivo, 'rb') as mysavedata:
+				ordenes = pickle.load(mysavedata)
+		except IOError as err:
+			print('File error: ' + str(err))
+		except pickle.PickleError as perr:
+			print('Pickling error: ' + str(perr))
+		finally:
+			self.borrarOrdenesGrabadas()
+			for comando in ordenes:
+				self.actualizarOrdenesGrabadas(comando[0],comando[1])
+			self.tiempo.set(ordenes[0][1])
+			
+	def borrarOrdenesGrabadas(self):
+		self.ordenesGrabadas = []
+		self.listaOrdenes.delete(0, self.listaOrdenes.size())
+		
+		
+	def handleList(self):
+		pass 
+		
 	
 				
 root=Tk()
